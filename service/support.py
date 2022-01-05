75ee5d66d22aea9a4bb5054fe0a79e6c7c2d7ef2
burasate <61697687+burasate@users.noreply.github.com>
2022-01-06 01:53:12 +0700

    Update support.py

--- service/support.py
+++ service/support.py
@@ -23,50 +23,7 @@ scriptsDir = formatPath(mayaAppDir + os.sep + 'scripts')
 projectDir = formatPath(scriptsDir + os.sep + 'BRSFacialRetargeter')
 userFile = formatPath(projectDir + os.sep + 'user')
 
-"""
-filepath = cmds.file(q=True, sn=True)
-filename = os.path.basename(filepath)
-raw_name, extension = os.path.splitext(filename)
-minTime = cmds.playbackOptions(q=True, minTime=True)
-maxTime = cmds.playbackOptions(q=True, maxTime=True)
-referenceList = cmds.ls(references=True)
-nameSpaceList = cmds.namespaceInfo(lon=True)
 
-userData = json.load(open(userFile, 'r'))
-data = {
-    'dateTime' : dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-    'timezone' : str( strftime('%z', gmtime()) ),
-    'year' : dt.datetime.now().strftime('%Y'),
-    'month' : dt.datetime.now().strftime('%m'),
-    'day' : dt.datetime.now().strftime('%d'),
-    'hour' : dt.datetime.now().strftime('%H'),
-    'email' : userData['email'],
-    'user' : getpass.getuser(),
-    'maya' : str(cmds.about(version=True)),
-    'ip' : str(urllib2.urlopen('https://v4.ident.me', timeout=5).read().decode('utf8')),
-    'version' : userData['version'],
-    'scene' : raw_name,
-    'timeUnit' : cmds.currentUnit(q=True, t=True),
-    'timeMin' : minTime,
-    'timeMax' : maxTime,
-    'duration' : maxTime - minTime,
-    'lastUpdate' : userData['lastUsedDate'],
-    'used' : userData['used'],
-    'isTrial' : int(userData['isTrial']),
-    'days' : userData['days'],
-    'registerDate' : userData['registerDate'],
-    'lastUsedDate' : userData['lastUpdate'],
-    'referenceCount': len(referenceList),
-    'nameSpaceList': ','.join(nameSpaceList),
-    'os' : str(cmds.about(operatingSystem=True))
-}
-
-url = 'https://hook.integromat.com/gnjcww5lcvgjhn9lpke8v255q6seov35'
-params = urllib.urlencode(data)
-conn = urllib.urlopen('{}?{}'.format(url, params))
-print(conn.read())
-#print(conn.info())
-"""
 
 # Supporter Coding
 print('Supporter is working now')
